12792ee449a956cb20f3bd3e633f462c065e509e
kzuabe <ai3ekzk@gmail.com>
2023-01-06 15:13:45 +0900

    feat: idTokenを使ったリクエスト形式に修正

--- lib/services/api/task.dart
+++ lib/services/api/task.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:http/http.dart' as http;
 import 'package:todolist_flutter_app/models/task.dart';
 
@@ -9,7 +11,9 @@ class TaskRepository {
   TaskRepository({required this.baseURL});
 
   Future<Tasks> fetchTasks() async {
-    final response = await http.get(Uri.parse('$baseURL/public/tasks'));
+    String token = await FirebaseAuth.instance.currentUser!.getIdToken();
+    final response = await http.get(Uri.parse('$baseURL/v1/tasks'),
+        headers: {HttpHeaders.authorizationHeader: 'Bearer $token'});
     if (response.statusCode == 200) {
       final parsed = jsonDecode(response.body).cast<Map<String, dynamic>>();
       return parsed.map<Task>((json) => Task.fromJson(json)).toList();
